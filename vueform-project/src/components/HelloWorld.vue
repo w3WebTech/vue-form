2d1cc82ce389f1eba4ca1b1998e14acde8a5dafe
w3WebTech <164163622+w3WebTech@users.noreply.github.com>
2024-10-12 10:37:19 +0530

    second fix

--- vueform-project/src/components/HelloWorld.vue
+++ vueform-project/src/components/HelloWorld.vue
@@ -1,17 +1,4 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
 
 <template>
-<div>  hello</div>
+  <div>hello world</div>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
